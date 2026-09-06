1a3f1798a50ebae535eba509c147bf8d82085033
zzz <49267799+Zzznopay@users.noreply.github.com>
2026-09-06 17:09:28 +0800

    Delete deploy directory

--- deploy/sql/00-init-other-dbs.sql
+++
@@ -1,3 +0,0 @@
--- 自动建库（仅在 MySQL 数据卷为空时执行一次）
-CREATE DATABASE IF NOT EXISTS `smart_property_property` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-CREATE DATABASE IF NOT EXISTS `smart_property_operation` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
